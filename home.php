bf1a3b6485226460e9f619939cd26ef765fc9343
sreehari61 <39291275+sreehari61@users.noreply.github.com>
2018-07-29 16:06:15 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>STUDENTS CORNER</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+	<!-- <link rel="stylesheet" type="text/css" href="assets/css/style.css"> -->
+	<link rel="stylesheet" type="text/css" href="assets/css/stylesheet.css">
+	<script type="text/javascript" src="assets/js/caroussol.js"></script>
+</head>
+<body>
+
+	<div class="wrapper">
+		<header>
+			<nav>
+				<div class="menu-icon">
+					<i class="fa fa-bars fa-2x"></i>
+				</div>
+				<div class="logo">
+					Students Corner
+				</div>
+				<div class="menu">
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>	
+			</nav>
+		</header>
+		<setion class="slider">
+			<ul class="slider-carousel" id="slider-carousel">
+				<li>
+					<h2>Wellcome to STUDENTS CORNER</h2>
+				</li>
+				<li>
+					<h2>Wellcome to STUDENTS CORNER</h2>
+				</li>
+				<li>
+					<h2>Wellcome to STUDENTS CORNER</h2>
+				</li>
+			</ul>
+		</setion>
+	</div>
+
+</body>
+</html>
